d7a164012254101a7f1f749772694eda68ed5a8a
DevinJP <djp0596@yahoo.com>
2017-01-11 09:45:32 -0500

    Update run.py

--- applicationVM/run.py
+++ applicationVM/run.py
@@ -4,7 +4,7 @@ import time
 
 host = "http://10.19.100.161:8080/post/"
 freq = str(sys.argv[1])
-runCommand = command = 'ffmpeg -f lavfi -i "sine=frequency='+freq+':sample_rate=8000:duration=3\" '+freq+'done.wav -loglevel quiet -y'
+runCommand = 'ffmpeg -f lavfi -i "sine=frequency='+freq+':sample_rate=8000:duration=3\" '+freq+'done.wav -loglevel quiet -y'
 command = 'curl -X POST -F "wav=@'+freq+'done.wav" 10.19.100.161:8080/post'
 while 1:
         os.system(runCommand)
